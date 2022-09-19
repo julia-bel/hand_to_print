b61cce219485e8c6e86b11b3bb73156264ca8a7c
Klaus1901 <Klaus1901>
2022-09-19 15:42:57 +0300

    #8 Добавлена отправка изображения от клиента

--- mobile_frontend/lib/services/HttpService.dart
+++ mobile_frontend/lib/services/HttpService.dart
@@ -1,15 +1,28 @@
-import 'dart:convert';
 import 'dart:developer';
-import 'dart:typed_data';
 import 'dart:ui';
+import 'package:http/http.dart' as http;
 
 class HttpService {
 
   static sendPicture(Image img) {
     img.toByteData(format: ImageByteFormat.png).then((value)
-    {
+    async {
       if (value != null) {
-        log(base64.encode(value.buffer.asUint8List()));
+        var server_url = 'http://192.168.1.93:8086/text_recognition';
+        var request = http.MultipartRequest('POST', Uri.parse(server_url));
+        request.files.add(
+            http.MultipartFile.fromBytes(
+                'text_image', value.buffer.asUint8List()
+            )
+        );
+
+        http.StreamedResponse response = await request.send();
+
+        if (response.statusCode == 201){
+          log(await response.stream.bytesToString());
+        } else {
+          log(response.reasonPhrase);
+        }
       }
     });
   }
