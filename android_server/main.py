0647e16e3f48c23b3fe795569fe32a2f8a64a2e0
Mihail A. Zholtikov <m.zholtikov@norsi-trans.ru>
2022-09-21 23:11:30 +0300

    #9 Добавил логи, перенес предобработку запроса в мидлвейр

--- android_server/main.py
+++ android_server/main.py
@@ -1,17 +1,21 @@
 from aiohttp import web
 
+from middleware import basic_validation
 from utils.common import set_logging
 from utils.config import Config
 from text_images_handler import TextImageActionHandler
 
 
 async def init_app():
-    app = web.Application()
+    middlewares = [basic_validation]
+    app = web.Application(middlewares=middlewares)
     app.config = Config
     app.base_logger = set_logging('Application server')
     app.add_routes([web.route(method='POST', path='/text_recognition', handler=TextImageActionHandler)])
+    app.base_logger.info(f'Start server on {Config.Api.bind_ip}:{Config.Api.bind_port}')
     return app
 
 
 if __name__ == '__main__':
     web.run_app(init_app(), host=Config.Api.bind_ip, port=Config.Api.bind_port)
+    print('Server was stopped')
---
+++ android_server/middleware.py
@@ -0,0 +1,19 @@
+from aiohttp import web
+
+from utils.common import multidict_to_dict
+
+
+@web.middleware
+async def basic_validation(request, handler):
+    logger = request.app.base_logger
+    logger.info(f'Start process request: {request.method} {request.match_info.route.resource} '
+                f'{request.match_info.route.resource.canonical}')
+    request.request_query_data = multidict_to_dict(request.query)
+    request_data = await request.post()
+    request.request_body_data = multidict_to_dict(request_data)
+    logger.info(f'Got request body data: {request.request_body_data},\n'
+                f'request query data: {request.request_query_data},\n'
+                f'{handler} start work')
+    response = await handler(request)
+    logger.info(f'{handler} finished processing request')
+    return response
