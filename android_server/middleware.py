5080d93c9eb0bf972499b8c9bfdb68775075f303
Mihail A. Zholtikov <m.zholtikov@norsi-trans.ru>
2022-09-30 10:55:11 +0300

    Поправил хендлер

--- android_server/middleware.py
+++ android_server/middleware.py
@@ -8,11 +8,11 @@ async def basic_validation(request, handler):
     logger = request.app.base_logger
     logger.info(f'Start process request: {request.method} {request.match_info.route.resource} '
                 f'{request.match_info.route.resource.canonical}')
-    request.request_query_data = multidict_to_dict(request.query)
+    handler.request_query_data = multidict_to_dict(request.query)
     request_data = await request.post()
-    request.request_body_data = multidict_to_dict(request_data)
-    logger.info(f'Got request body data: {request.request_body_data},\n'
-                f'request query data: {request.request_query_data},\n'
+    handler.request_body_data = multidict_to_dict(request_data)
+    logger.info(f'Got request body data: {handler.request_body_data},\n'
+                f'request query data: {handler.request_query_data},\n'
                 f'{handler} start work')
     response = await handler(request)
     logger.info(f'{handler} finished processing request')
--- android_server/text_images_handler.py
+++ android_server/text_images_handler.py
@@ -2,9 +2,6 @@ import io
 
 from aiohttp import web
 
-from utils.common import multidict_to_dict
-from PIL import Image
-
 
 class TextImageActionHandler(web.View):
     def __init__(self, request):
@@ -12,14 +9,10 @@ class TextImageActionHandler(web.View):
         self.logger = request.action_logger if hasattr(request, 'action_logger') else request.app.base_logger
 
     async def post(self):
-        request_data = await self.request.post()
-        request_body_data = multidict_to_dict(request_data)
-        if request_body_data.get('text_image'):
+        if self.request_body_data.get('text_image'):
             self.logger.info(f'TextImageActionHandler succesfully load images')
-            for image in request_body_data['text_image']:
-                self.logger.info(f'Start getting text from {request_body_data["text_image"].index(image)+1} image')
-                img = Image.open(io.BytesIO(image))
-                img.show()
-
+            images = [io.BytesIO(image) for image in self.request_body_data['text_image']]
+            # text = ModelManager.preprocess(images)
+        text = 'ahahahahahaha, it is some text'
         self.logger.info('All images successfully proccessed')
-        return web.json_response(data=[1, 2, 3], status=201)
+        return web.json_response(data={'text': text}, status=201)
