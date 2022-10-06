c4e24cee228d8610379fbae4a74171ffcaf27cb0
Julia Belikova <yulyawh@gmail.com>
2022-10-07 01:39:34 +0300

    #24 Изменен формат входных данных

--- android_server/ocr_model/manager.py
+++ android_server/ocr_model/manager.py
@@ -1,9 +1,10 @@
 import re
-from typing import Any, Dict, List, Optional
-
 import cv2
-import numpy as np
 import torch
+import numpy as np
+
+from typing import Any, Dict, List, Optional
+from PIL import Image
 
 
 class ModelManager:
@@ -14,13 +15,12 @@ class ModelManager:
         self.device = config["device"]
         self.preprocess_config = config["preprocess_config"]
 
-    def preprocess(self, paths: List[str]) -> Dict[str, np.ndarray]:
-        return self.apply_preprocessing(self.load_images(paths))
+    def preprocess(self, images: List[Image], n=16) -> Dict[str, np.ndarray]:
+        l = len(images)
+        for i in range(0, l, n):
+            yield self.apply_preprocessing(images[i:min(i + n, l)])
 
-    def load_images(self, paths: List[str]) -> List[np.ndarray]:
-        return [cv2.imread(path) for path in paths]
-    
-    def apply_preprocessing(self, images: List[np.ndarray]) -> Dict[str, np.ndarray]:
+    def apply_preprocessing(self, images: List[Image]) -> Dict[str, np.ndarray]:
 
         def pad_image_width_right(img: np.ndarray, new_width: int, padding_value: int) -> np.ndarray:
             h, w, c = img.shape
@@ -35,12 +35,13 @@ class ModelManager:
             pad_bottom = np.ones((pad_height, w, c)) * padding_value
             img = np.concatenate([img, pad_bottom], axis=0)
             return img
-        
+
         samples = {
             "image": [],
             "reduced_shape": []
         }
         for i in range(len(images)):
+            images[i] = np.array(images[i])
             for preprocessing in self.preprocess_config["preprocessings"]:
                 if preprocessing["type"] == "dpi":
                     ratio = preprocessing["target"] / preprocessing["source"]
@@ -70,6 +71,8 @@ class ModelManager:
                         images[i],
                         self.preprocess_config["padding"]["min_width"],
                         self.preprocess_config["padding_value"])
+            if "normalize" in self.params["config"]["constraints"]:
+                sample["img"] = (sample["img"] - sample["img"].mean()) / sample["img"].std()
             samples["image"].append(images[i])
         samples["image"] = np.array(samples["image"])
         samples["reduced_shape"] = np.array(samples["reduced_shape"])
@@ -77,7 +80,7 @@ class ModelManager:
 
     def postprocess(self, ind_x: List[torch.Tensor]) -> List[str]:
 
-        def ind_to_str(ind: List[torch.Tensor], oov_symbol: Any = None):
+        def ind_to_str(ind: List[torch.Tensor], oov_symbol: Any = None) -> str:
             if oov_symbol is not None:
                 res = []
                 for i in ind:
@@ -89,7 +92,7 @@ class ModelManager:
                 res = [self.charset[i] for i in ind]
             return "".join(res)
 
-        def ctc_remove_successives(ind: torch.Tensor):
+        def ctc_remove_successives(ind: torch.Tensor) -> List[int]:
             res = []
             for j in ind:
                 for i in j:
@@ -100,13 +103,14 @@ class ModelManager:
 
         str_x = []
         for lines_token in ind_x:
-            list_str = [ind_to_str(ctc_remove_successives(p), oov_symbol="") 
+            list_str = [ind_to_str(ctc_remove_successives(p), oov_symbol="")
                         if p is not None else "" for p in lines_token]
-            str_x.append(re.sub("( )+", " ", " ".join(list_str).strip(" ")))
-        return [x.replace(" ", "") for x in str_x]
-    
+            str_x.append(re.sub("(\n)+", "\n", "\n".join(list_str).strip("\n")))
+        return str_x
+
     def append_predictions(
-        self, pg_preds: List[List[torch.Tensor]], 
+        self,
+        pg_preds: List[List[torch.Tensor]],
         line_preds: List[Optional[torch.Tensor]]
     ) -> List[List[torch.Tensor]]:
         for i, lp in enumerate(line_preds):
@@ -120,33 +124,25 @@ class ModelManager:
 
         for i in range(batch_size):
             line_pred[i] = [(torch.argmax(lp, dim=0).detach().cpu())[:x_reduced_len[j]] for j, lp in enumerate(probs[i])]
-        
+
         for i in range(len(line_pred)):
             line_pred[i] = [l if probs[i] != None else None for j, l in enumerate(line_pred[i])]
-    
-        preds = [[] for _ in range(batch_size)] 
+
+        preds = [[] for _ in range(batch_size)]
         preds = self.append_predictions(preds, line_pred)
         return preds
 
-    def evaluate(self, paths: List[str]) -> List[str]:
-        probs = self.get_probs(paths) 
-        sample = self.preprocess(paths)
-        x_reduced_len = [shape[1] for shape in sample["reduced_shape"]]
-        x_reduced_len = torch.tensor(x_reduced_len).int().to(self.device)
-        preds = self.decode(probs, x_reduced_len)   
-        return self.postprocess(preds)
-
-    def predict(self, paths: List[str]) -> List[str]:
+    def evaluate(self, batch) -> List[List[torch.Tensor]]:
         with torch.no_grad():
-            predictions = self.evaluate(paths)
-        return predictions
-    
-    def get_probs(self, paths: List[str]) -> List[str]:
-        sample = self.preprocess(paths)
-        with torch.no_grad():
-            x = torch.from_numpy(sample["image"])
-            x_reduced_len = [shape[1] for shape in sample["reduced_shape"]]
+            x = torch.from_numpy(batch["image"])
             x = torch.tensor(x).float().permute(0, 3, 1, 2).to(self.device)
+            x_reduced_len = [shape[1] for shape in batch["reduced_shape"]]
             x_reduced_len = torch.tensor(x_reduced_len).int().to(self.device)
             probs = self.model(x, x_reduced_len)
-        return probs
+        preds = self.decode(probs, x_reduced_len)
+        return preds
+
+    def predict(self, images: List[Image]) -> List[str]:
+        preds = [p for batch in self.preprocess(images)
+                for p in self.postprocess(self.evaluate(batch))]
+        return preds
